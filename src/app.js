d18df156e2ebecf7d88e5c727a0aa4e33910ee10
ppalone <pranjalalone@gmail.com>
2020-11-14 20:53:04 +0530

    Few tweaks

--- src/app.js
+++ src/app.js
@@ -6,25 +6,16 @@ canvas.height = window.innerHeight;
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
-window.addEventListener('resize', resizeCanvas, false);
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  WIDTH = window.innerWidth;
-  HEIGHT = window.innerHeight;
-}
-
-resizeCanvas();
-
 const ctx = canvas.getContext('2d', { alpha: false });
 
 const fireworks = [];
 const particles = [];
 
-const GRAVITY = 0.005;
+const GRAVITY = 0.006;
 
-const colors = ['cyan', 'yellow', 'orange', 'white', 'lime'];
+console.log(
+  "Wishing you a very Happy & Prosperous Diwali 🎉, Don't forget to leave a star ⭐ at https://github.com/ppalone/happy-diwali"
+);
 
 class Firework {
   constructor(fx, fy, color, speed) {
@@ -40,15 +31,20 @@ class Firework {
   update() {
     this.draw();
     if (this.y < this.fy) {
-      const PARTICLE_COUNT = 200;
+      const PARTICLE_COUNT = 100;
       const ANGLE = (Math.PI * 2) / PARTICLE_COUNT;
 
       for (let i = 0; i < PARTICLE_COUNT; i++) {
         particles.push(
-          new Particle(this.fx, this.fy, this.color, {
-            x: Math.cos(ANGLE * i) * Math.random(),
-            y: Math.sin(ANGLE * i) * Math.random(),
-          })
+          new Particle(
+            this.fx,
+            this.fy,
+            `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`,
+            {
+              x: Math.cos(ANGLE * i) * Math.random(),
+              y: Math.sin(ANGLE * i) * Math.random(),
+            }
+          )
         );
       }
       this.blown = true;
@@ -74,15 +70,15 @@ class Particle {
   }
 
   draw() {
-    ctx.save();
-    ctx.globalAlpha = Math.max(this.alpha, 0);
+    // ctx.save();
+    ctx.globalAlpha = this.alpha < 0 ? 0 : this.alpha;
     ctx.beginPath();
     ctx.arc(this.x, this.y, 2, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
     ctx.strokeStyle = 'transparent';
     ctx.fill();
     ctx.stroke();
-    ctx.restore();
+    // ctx.restore();
   }
 
   update() {
@@ -95,16 +91,33 @@ class Particle {
 }
 
 function animate() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.50)';
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
   fireworks.forEach((firework, idx) => {
     if (firework.blown) {
+      // Remove the old when blown
       fireworks.splice(idx, 1);
+      // Spawn New Firework
+      let pos = {
+        x: 30 + Math.floor(Math.random() * (WIDTH - 30 * 2)),
+        y: 30 + Math.floor(Math.random() * (HEIGHT / 2)),
+      };
+      // let color = colors[Math.floor(Math.random() * colors.length)];
+      let speed = 4 + Math.floor(Math.random() * 4);
+      fireworks.push(
+        new Firework(
+          pos.x,
+          pos.y,
+          `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`,
+          speed
+        )
+      );
     } else {
       firework.update();
     }
   });
+
   particles.length > 0 &&
     particles.forEach((particle, idx) => {
       if (particle.alpha <= 0) {
@@ -117,14 +130,21 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-animate();
-
-setInterval(function () {
+function init() {
   let pos = {
     x: 30 + Math.floor(Math.random() * (WIDTH - 30 * 2)),
     y: 30 + Math.floor(Math.random() * (HEIGHT / 2)),
   };
-  let color = colors[Math.floor(Math.random() * colors.length)];
   let speed = 4 + Math.floor(Math.random() * 4);
-  fireworks.push(new Firework(pos.x, pos.y, color, speed));
-}, 2000);
+  fireworks.push(
+    new Firework(
+      pos.x,
+      pos.y,
+      `hsl(${Math.floor(Math.random() * 360)},50%, 50%)`,
+      speed
+    )
+  );
+}
+
+init();
+animate();
